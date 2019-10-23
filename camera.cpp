023f0639816f3d1e65db21d03cf99ff2070aa9ea
harv <harveyives@gmail.com>
2019-10-23 16:41:44 +0100

    removed a bunch of nonsense from the camera

--- camera.cpp
+++ camera.cpp
@@ -7,32 +7,32 @@
 
 Camera::Camera(Vertex eye, Vertex look, Vector up, double d, float fov, int height, int width) {
     // TODO clear this stuff up
-    Camera::eye = eye;
-    Camera::look = look;
-    Camera::up = up;
-    Camera::d = d;
+    this->eye = eye;
+    this->look = look;
+    this->up = up;
+    this->d = d;
 
-    Camera::w = Vector(look.x, look.y, look.z, eye.x, eye.y, eye.z);
+    w = Vector(look.x, look.y, look.z, eye.x, eye.y, eye.z);
     w.normalise();
 
-    Camera::up.cross(Camera::w, Camera::u);
+    up.cross(w, u);
     u.normalise();
 
-    Camera::w.cross(Camera::u, Camera::v);
+    w.cross(u, v);
 
-    Camera::fov_radians = M_PI * (fov / 2) / 180;
-    Camera::aspect_ratio = height / width;
+    fov_radians = M_PI * (fov / 2) / 180;
+    aspect_ratio = height / width;
 
-    Camera::half_width = tan(fov_radians);
-    Camera::half_height = aspect_ratio * half_width;
+    half_width = tan(fov_radians);
+    half_height = aspect_ratio * half_width;
 
-    Camera::pixel_width = half_width * 2 / (width - 1.0);
-    Camera::pixel_height = half_height * 2 / (height - 1.0);
+    pixel_width = half_width * 2 / (width - 1.0);
+    pixel_height = half_height * 2 / (height - 1.0);
 }
 
 Vector Camera::get_ray_direction(int x, int y) {
-    float xv = (x * Camera::pixel_width) - Camera::half_width;
-    float yv = (y * Camera::pixel_height) - Camera::half_height;
+    float xv = (x * pixel_width) - half_width;
+    float yv = (y * pixel_height) - half_height;
 
     Vector D = u * xv + v * yv - w * d;
     D.normalise();
